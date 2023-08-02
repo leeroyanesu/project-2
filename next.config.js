1431349fd4c63b8498d014d5011484b207901a23
Leeroy Chako <leeroychako@gmail.com>
2023-08-02 09:54:36 +0200

    main

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   
+    i18n:{
+        locales:['en-US','de','fr'],
+        defaultLocale:'en-US',
+        localeDetection:false
+    }
+}
 
 module.exports = nextConfig
